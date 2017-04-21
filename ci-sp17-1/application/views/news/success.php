e60629aa9120e4e3358412c354b69463a88dbe68
KerstinGraudins <kgraud01@seattlecentral.edu>
2017-04-20 17:44:43 -0700

    this added themes to the ci application

--- ci-sp17-1/application/views/news/success.php
+++ ci-sp17-1/application/views/news/success.php
@@ -1,3 +1,12 @@
+<?php
+//application/views/news/success.php
 
+$this->load->view($this->config->item('theme') . 'header');
+
+?>
 <p><b>you have successfully created a news item!</b></p>
 <p>now I suppose you want to see it?</p>
+<?php
+
+$this->load->view($this->config->item('theme') . 'footer');
+?>
